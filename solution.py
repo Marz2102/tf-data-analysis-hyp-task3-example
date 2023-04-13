7563fc27e77078b6428aae6085bbfcad78e7a4a8
Marz2102 <129897990+Marz2102@users.noreply.github.com>
2023-04-13 15:31:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import ttest_1samp
+from statsmodels.stats.weightstats import ztest
 
 
 chat_id = 682673597 # Ваш chat ID, не меняйте название переменной
 
 def solution(data) -> bool:
-    E = 500
-    t_stat, p_value = ttest_1samp(data, E)
     p = 0.06
-    if p_value/2 < p and t_stat < 0:
-        return True
-    else:
-        return False
+    stat, pval  = ztest(x, value=500, alternative='larger')
+    return pval < p
